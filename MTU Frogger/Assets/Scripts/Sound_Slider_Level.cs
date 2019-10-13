2c247e8ca4abf9cb40b950d2e11e6def765e5454
Slyleaves <55101313+Slyleaves@users.noreply.github.com>
2019-10-13 08:23:50 -0400

    Scripts - Slider
    Added a scripts folder, and added a script for the sound slider for the options screen

---
+++ MTU Frogger/Assets/Scripts/Sound_Slider_Level.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Sound_Slider_Level : MonoBehaviour
+{
+    public Text text;
+    public Slider slider;
+
+        public void UpdateText ()
+    {
+        text.text = slider.value.ToString();
+    }
+
+
+}
