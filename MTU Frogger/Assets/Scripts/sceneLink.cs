48775b575e8a99cdad66fca6df961820eb946a58
bdsearle <55106822+bdsearle@users.noreply.github.com>
2019-10-14 15:33:55 -0400

    Button working
    Added options and start button functionality

---
+++ MTU Frogger/Assets/Scripts/sceneLink.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneLink : MonoBehaviour
+{
+    public void SceneLoader(int SceneIndex)
+    {
+
+
+        SceneManager.LoadScene(SceneIndex);
+
+    } 
+}
