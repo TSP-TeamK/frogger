d0bc4d7b5db4723bea24babd5ff51144677e02fc
Slyleaves <55101313+Slyleaves@users.noreply.github.com>
2019-10-14 12:48:26 -0400

    Hooked slider to audio

---
+++ MTU Frogger/Assets/Scripts/VolumeLevelChange.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeLevelChange : MonoBehaviour
+{
+    public Slider audioSlider;
+  public void OnValueChanged()
+    {
+        AudioListener.volume = audioSlider.value;
+    }
+}
