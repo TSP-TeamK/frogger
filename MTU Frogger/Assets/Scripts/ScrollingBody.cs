6b381a712943ca627e1f4b60f82101eb58ef9e1e
Slyleaves <55101313+Slyleaves@users.noreply.github.com>
2019-10-15 10:56:51 -0400

    Level One - add Car and Road
    Added a temporary car and road sprite as well as a script that controls the speed and position of the car. Car will roll across the screen and reset back around when it goes off screen

---
+++ MTU Frogger/Assets/Scripts/ScrollingBody.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingBody : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+    private Vector2 newPos = new Vector2(-12.0f, 0.0f);
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>(); // get reference
+        //set velocity
+        rb2d.velocity = new Vector2(2.0f, 0); //could use 1.5f as a public variable later 
+                                                // so all cars move same speed
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (GameControl.instance.gameOver = true) {rb2d.velocity = Vector2.zero}
+        //We don't currently have a gameOver thing
+
+        if( rb2d.position.x >= 13f)
+        {
+            rb2d.position = newPos;
+        }
+    }
+}
