b4559561a1369b5aa0fcf6698a303ffa02a000d9
Amakuro <ruoxit@mtu.edu>
2019-10-28 12:49:29 -0400

    Added Tile System and Player Prefab
    - Implemented Tile System
    - Made Player as prefab so it can be easily dragged into scene
    - Updated some animation so it fixes the still movement while player actually moves

---
+++ MTU Frogger/Assets/Editor/TestScript.cs
@@ -0,0 +1,14 @@
+﻿
+using UnityEditor;
+using System.Collections;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+
+public class TestScript
+{
+    [UnityTest]
+    public void test()
+    {
+       
+    }
+}
