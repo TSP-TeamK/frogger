0d8eb2df287aabd5b285c83fe1f297c2a47858e5
nbmoon <nbmoon@mtu.edu>
2019-11-04 12:55:41 -0500

    Added movement limits!

---
+++ MTU Frogger/Assets/Scripts/GlobalVariables.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GlobalVariables
+{
+	public static float negXvalue = -12f;//holds maximum distance the character can move to the left
+	public static float Xvalue = 12f;//holds maximum distance the character can move to the right
+	public static float negYvalue = -4.5f;//holds maximum distance the character can move to the bottom
+	public static float Yvalue = 0f;//holds maximum distance the character can move to the top
+}
