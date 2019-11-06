bcba4d34f53ac9f0731f1e7ae79df2be0e54aa13
Slyleaves <55101313+Slyleaves@users.noreply.github.com>
2019-11-06 12:37:54 -0500

    Revert "Added movement limits!"
    This reverts commit 0d8eb2df287aabd5b285c83fe1f297c2a47858e5.

---
+++ MTU Frogger/Assets/Scripts/Movement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    //a rigid body for the frog
+    Rigidbody2D frog = new Rigidbody2D();
+    //set speed for frog
+    public float speed = 2.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //get the frog Rigidbody2D
+        frog = GetComponent<Rigidbody2D>();
+	speed = 1.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //get right key
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+		Vector2 movement = new Vector2(speed, 0);
+            frog.AddForce(movement);
+        }
+        //get left key
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            Vector2 movement = new Vector2(-speed, 0);
+            frog.AddForce(movement);
+        }
+        //get up key
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            Vector2 movement = new Vector2(0, speed);
+            frog.AddForce(movement);
+        }
+        //get down key
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            Vector2 movement = new Vector2(0, -speed);
+            frog.AddForce(movement);
+        }
+    }
+}
