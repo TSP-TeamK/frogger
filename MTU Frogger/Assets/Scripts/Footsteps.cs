a3b5ce0365b0817f8a2d093e624c7bc3bb04d587
sushi <ruoxit@mtu.edu>
2019-12-02 08:45:27 -0500

    Added Sound Effect Toggle
    Sound Effect Toggle controls the footstep for player

--- MTU Frogger/Assets/Scripts/Footsteps.cs
+++ MTU Frogger/Assets/Scripts/Footsteps.cs
@@ -1,26 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Footsteps : MonoBehaviour
 {
     FrogMovement movement;
- 
+    public GameObject soundToggle;
+    Toggle toggle;
+
     // Start is called before the first frame update
     void Start()
     {
         movement = GetComponent<FrogMovement>();
+        toggle = soundToggle.GetComponent<Toggle>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        //if player is moving and toggle setting is on
+        //play footstep sound effect
         if (movement.playerMoving == true && movement.moveSpeed > 0.5f
-            && GetComponent<AudioSource>().isPlaying == false)
+            && GetComponent<AudioSource>().isPlaying == false && PlayerPrefs.GetInt("ToggleSetting") == 1)
         {
             GetComponent<AudioSource>().volume = Random.Range(0.5f, 1);
             GetComponent<AudioSource>().pitch = Random.Range(0.8f, 1.1f);
             GetComponent<AudioSource>().Play();
         }
+        //if toggle setting is off, mute sound effect
+        if (PlayerPrefs.GetInt("ToggleSetting") == 0)
+        {
+            GetComponent<AudioSource>().volume = 0;
+            GetComponent<AudioSource>().pitch = 0;
+        }
     }
+
 }
---
+++ MTU Frogger/Assets/Scripts/SoundEffectToggle.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundEffectToggle : MonoBehaviour
+{
+    public Text text;
+    public Toggle toggle;
+    public GameObject player;
+
+    void Start()
+    {
+        //set sound effect on when game starts
+        if (PlayerPrefs.GetInt("ToggleSetting") == 1)
+        {
+            text.text = "SE ON";
+            toggle.isOn = true;
+        }
+        else
+        {
+            text.text = "SE OFF";
+            toggle.isOn = false;
+        }
+    }
+
+    public void UpdateText()
+    {
+        if (toggle.isOn)
+        {
+            text.text = "SE ON";
+        }
+        else
+        {
+            text.text = "SE OFF";
+        }
+    }
+
+    public void saveVolumeLevel()
+    {
+        if (toggle.isOn)
+        {
+            PlayerPrefs.SetInt("ToggleSetting", 1);
+        }
+        else
+        {
+            PlayerPrefs.SetInt("ToggleSetting", 0);
+        }
+    }
+
+}
