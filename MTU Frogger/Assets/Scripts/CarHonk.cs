8e9d502fc95da0eedf9598d117c72c0e6c93962c
sushi <ruoxit@mtu.edu>
2019-12-05 17:54:31 -0500

    Added Randomly Played Car Honk Sound

---
+++ MTU Frogger/Assets/Scripts/CarHonk.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarHonk : MonoBehaviour
+{
+
+    GameObject player;
+    private float timeToGo;
+
+    private void Awake()
+    {
+        player = GameObject.Find("character");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeToGo = Time.fixedTime + Random.Range(1.0f, 3.0f);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Time.fixedTime >= timeToGo)
+        {
+            {
+                //if player is around the car then play honk noises randomly
+                if (player.transform.position.y > transform.position.y - 10 ||
+                player.transform.position.y < transform.position.y + 10)
+                {
+                    //randomly play car noises
+                    if (Random.Range(1.0f, 50.0f) < 25)
+                    {
+                        GetComponent<AudioSource>().volume = Random.Range(0.6f, 1.0f);
+                        GetComponent<AudioSource>().pitch = Random.Range(0.8f, 1.2f);
+                        GetComponent<AudioSource>().Play();
+                    }
+                    else
+                    {
+                        GetComponent<AudioSource>().Pause();
+                    }
+                }
+                else
+                {
+                    GetComponent<AudioSource>().Pause();
+                }
+            }
+            timeToGo = Time.fixedTime + Random.Range(3.0f, 5.0f);
+        }
+    }
+
+}
