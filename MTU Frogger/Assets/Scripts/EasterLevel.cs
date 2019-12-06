b8302ca9b217b13158d0b81cb179dfad243f9e3c
sushi <ruoxit@mtu.edu>
2019-12-06 13:59:49 -0500

    Updated Easter Egg Level Select
    - If all the coins are collected, then easter egg level will appear in select menu and main menu will appear text

---
+++ MTU Frogger/Assets/Scripts/EasterLevel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EasterLevel : MonoBehaviour
+{
+    public Button button;
+    public Text tryEasterEggLevel;
+
+    void Start()
+    {
+        if (PlayerPrefs.GetInt("Score") >= 400)
+        {
+            button.gameObject.SetActive(true);
+            tryEasterEggLevel.gameObject.SetActive(true);
+        }
+    }
+}
